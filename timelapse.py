01409c3324607b130477bd46d7d32a59c650d6cb
Dulus0 <Dulus0@users.noreply.github.com>
2021-04-16 13:51:28 +0200

    Revert "Revert "image name starts again from 0""
    This reverts commit 337a54dcfe0f51d9291cdafc23c62759e3594680.

--- timelapse.py
+++ timelapse.py
@@ -67,7 +67,7 @@ def capture_image():
         set_camera_options(camera)
 
         # Capture a picture.
-        camera.capture(dir + '/image{0:08d}.jpg'.format(image_number+1))
+        camera.capture(dir + '/image{0:08d}.jpg'.format(image_number))
         sleep(1)
         camera.close()
 
